d00395c1119d4d7a4e9a2982862bbb71e0fb182b
Lola <lola@egherman.com>
2020-06-11 17:00:06 -0500

    remove audio from core

--- cogs/core.py
+++ cogs/core.py
@@ -4,7 +4,7 @@ from audio import MixedAudioSource
 
 
 class Core(commands.Cog):
-    def __init__(self, bot, audio):
+    def __init__(self, bot):
         self.bot = bot
         self.vc = None
         self.audio = MixedAudioSource()
