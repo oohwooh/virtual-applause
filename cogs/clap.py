f4e128edf353d0353261431b89a1f7cf90a81337
Lola <lola@egherman.com>
2020-06-11 16:43:48 -0500

    fix stuff

--- cogs/clap.py
+++ cogs/clap.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 
 class Clap(commands.Cog):
-    def __init__(self, bot, audio):
+    def __init__(self, bot):
         self.bot = bot
         self.core = self.bot.get_cog('Core')
         self.files = ['./claps/'+f for f in os.listdir('./claps')]
@@ -16,17 +16,17 @@ class Clap(commands.Cog):
             return True
         return False
 
-    @commands.Cog.listener
+    @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         if str(payload.emoji) == self.emoji and payload.user_id != self.bot.user.id:
             self.play()
 
-    @commands.Cog.listener
+    @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
         if str(payload.emoji) == self.emoji:
             self.play()
 
-    @commands.Cog.listener
+    @commands.Cog.listener()
     async def on_message(self, message):
         triggers = ['clap', self.emoji]
         if any(trigger in message.content.lower() for trigger in triggers):
