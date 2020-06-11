e4c9cbe8188d576a3cef8e72289b31c6f8fb95e8
Lola <lola@egherman.com>
2020-06-11 16:11:35 -0500

    add clap cog

---
+++ cogs/clap.py
@@ -0,0 +1,33 @@
+import os
+import random
+from discord.ext import commands
+
+
+class Clap(commands.Cog):
+    def __init__(self, bot, audio):
+        self.bot = bot
+        self.core = self.bot.get_cog('Core')
+        self.files = ['./claps/'+f for f in os.listdir('./claps')]
+        self.emoji = '👏'
+
+    def play(self, message=None):
+        if self.core.vc is not None:
+            self.core.add_stream(random.choice(self.files))
+            if message:
+                await message.add_reaction(self.emoji)
+
+    @commands.Cog.listener
+    async def on_raw_reaction_add(self, payload):
+        if str(payload.emoji) == self.emoji and payload.user_id != self.bot.user.id:
+            self.play()
+
+    @commands.Cog.listener
+    async def on_raw_reaction_remove(self, payload):
+        if str(payload.emoji) == self.emoji:
+            self.play()
+
+    @commands.Cog.listener
+    async def on_message(self, message):
+        triggers = ['clap', self.emoji]
+        if any(trigger in message.content.lower() for trigger in triggers):
+            self.play(message)
